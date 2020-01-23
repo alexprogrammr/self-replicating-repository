d1b370ae1828660c1bfd16f2ac4a4d460471fb16
alexprogrammr <alex.programmr@gmail.com>
2020-01-23 22:17:15 +0200

    Provide default values for some environment variables

--- config.py
+++ config.py
@@ -2,13 +2,14 @@ import os
 
 
 class Config:
-    SECRET_KEY = os.urandom(24)
+    SECRET_KEY = os.environ.get('SECRET_KEY') or os.urandom(24)
 
-    GITHUB_USERNAME = os.environ.get('GITHUB_USERNAME')
-    GITHUB_REPONAME = os.environ.get('GITHUB_REPONAME')
+    GITHUB_USERNAME = os.environ.get('GITHUB_USERNAME', 'alexprogrammr')
+    GITHUB_REPONAME = os.environ.get('GITHUB_REPONAME', 'self-replicating-repository')
+
+    GITHUB_API_BASE_URL = os.environ.get('GITHUB_API_BASE_URL', 'https://api.github.com')
+    GITHUB_AUTHORIZE_URL = os.environ.get('GITHUB_AUTHORIZE_URL', 'https://github.com/login/oauth/authorize')
+    GITHUB_ACCESS_TOKEN_URL = os.environ.get('GITHUB_ACCESS_TOKEN_URL', 'https://github.com/login/oauth/access_token')
 
     GITHUB_CLIENT_ID = os.environ.get('GITHUB_CLIENT_ID')
     GITHUB_CLIENT_SECRET = os.environ.get('GITHUB_CLIENT_SECRET')
-    GITHUB_API_BASE_URL = os.environ.get('GITHUB_API_BASE_URL')
-    GITHUB_AUTHORIZE_URL = os.environ.get('GITHUB_AUTHORIZE_URL')
-    GITHUB_ACCESS_TOKEN_URL = os.environ.get('GITHUB_ACCESS_TOKEN_URL')
